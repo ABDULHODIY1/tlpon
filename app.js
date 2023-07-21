f2909197346afcd75c713532d557beb9104a4ebe
ABDULHODIY1 <muhiddinovabdulhodiy2@gmail.com>
2023-07-21 21:07:18 +0500

    head

---
+++ app.js
@@ -0,0 +1,6 @@
+function toogle() {
+    var blur = document.querySelector('#c');
+    blur.classList.toggle('active');
+    var popup = document.querySelector('.span_container');
+    popup.classList.toggle('active');
+}
